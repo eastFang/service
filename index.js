85ecad53fed13691054a53f9d0c9d2000cd80978
方镇东 <fangzhendong@yiwise.com>
2019-10-22 17:41:37 +0800

    add koastatic

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 const Koa = require('koa')
 const KoaBody = require('koa-body')
 const KoaRouter = require('koa-router')
-const koaCors = require('@koa/cors')
+const KoaStatic = require('koa-static')
 const path = require('path')
 const fs = require('fs')
 const app = new Koa()
 const router = KoaRouter()
 
-const PORT = 50000
-app.use(KoaBody({
+const LISTEN_PORT = 50000
+const RES_STATIC_PATH_PREFIX = 'http://localhost:${LISTEN_PORT}/images/'
+
+app.use(KoaBody({ // 让koa读取 request body, 支持multipart
   multipart: true,
   formidable: {
     uploadDir: path.resolve(__dirname, './static/images')
   }
 }))
+
+app.use(KoaStatic(
+  path.resolve(__dirname, './static')
+))
+app.use((ctx, next) => {
+  if (ctx.method === 'OPTIONS') {
+    ctx.set('Access-Control-Allow-Origin', '*')
+    ctx.status = 204
+  } else {
+    next()
+  }
+})
+
 router.post('/upload', ctx => {
-  console.log('/upload我进来了')
+  ctx.set('Access-Control-Allow-Origin', '*')
   const files = Array.from(ctx.request.files.file)
+  const resFiles = []
   files.forEach(file => {
     const nextPath = file.path.substring(0, file.path.lastIndexOf('/')) + '/' + file.name
     fs.renameSync(file.path, nextPath)
+    resFiles.push(`${RES_STATIC_PATH_PREFIX}${file.name}`)
   })
-  ctx.body = '上传成功'
+  ctx.body = {
+    data: resFiles
+  }
 })
 
-app.use(koaCors({
-  origin: '*'
-}))
+router.post('/getUsers', ctx => {
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.status = 200
+  ctx.body = '4'
+})
 
 app.use(router.routes())
   .use(router.allowedMethods())
 
-app.listen(PORT, () => {
-  console.log(`正在监听${PORT}端口`)
+app.listen(LISTEN_PORT, () => {
+  console.log(`正在监听${LISTEN_PORT}端口`)
 })
